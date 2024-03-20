40760157cebe248d813e0dd3b265d1cc9da6296a
nathgoutam93 <nathgoutam93@gmail.com>
2024-03-20 15:08:38 +0530

    if no bet or only one bet play genuine

--- apps/prediction-backend/src/games/games.service.ts
+++ apps/prediction-backend/src/games/games.service.ts
@@ -52,146 +52,160 @@ export class GamesService {
   }
 
   async findWinningNumber(game_id: number) {
-    const defaultMultiplier = parseFloat(
-      process.env.DEFAULT_MULTIPLIER as string,
-    );
-    const numberMultiplier = parseFloat(
-      process.env.NUMBER_MULTIPLIER as string,
-    );
-
-    const betsOnNumbers = {
-      0: 0,
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0,
-      6: 0,
-      7: 0,
-      8: 0,
-      9: 0,
-    };
-
-    const betsOnSizeNColors = {
-      small: 0,
-      big: 0,
-      red: 0,
-      green: 0,
-      violet: 0,
-    };
-
-    const betAmountSum_groupBy_prediction =
-      await this.databaseService.bet.groupBy({
-        by: ['prediction'],
-        where: {
-          gameId: game_id,
-        },
-        _sum: {
-          amount: true,
-        },
-      });
-
-    for (const betGroup of betAmountSum_groupBy_prediction) {
-      if (isNaN(parseInt(betGroup.prediction))) {
-        betsOnSizeNColors[betGroup.prediction] = betGroup._sum.amount;
-      } else {
-        betsOnNumbers[betGroup.prediction] = betGroup._sum.amount;
-      }
-    }
-
-    const betsOnNumberByColorNSize = {
-      0:
-        Number(betsOnSizeNColors[this.numbers[0].size]) +
-        Number(betsOnSizeNColors[this.numbers[0].color[0]]) +
-        Number(betsOnSizeNColors[this.numbers[0].color[1]]),
-      1:
-        Number(betsOnSizeNColors[this.numbers[1].size]) +
-        Number(betsOnSizeNColors[this.numbers[1].color[0]]),
-      2:
-        Number(betsOnSizeNColors[this.numbers[2].size]) +
-        Number(betsOnSizeNColors[this.numbers[2].color[0]]),
-      3:
-        Number(betsOnSizeNColors[this.numbers[3].size]) +
-        Number(betsOnSizeNColors[this.numbers[3].color[0]]),
-      4:
-        Number(betsOnSizeNColors[this.numbers[4].size]) +
-        Number(betsOnSizeNColors[this.numbers[4].color[0]]),
-      5:
-        Number(betsOnSizeNColors[this.numbers[5].size]) +
-        Number(betsOnSizeNColors[this.numbers[5].color[0]]) +
-        Number(betsOnSizeNColors[this.numbers[5].color[1]]),
-      6:
-        Number(betsOnSizeNColors[this.numbers[6].size]) +
-        Number(betsOnSizeNColors[this.numbers[6].color[0]]),
-      7:
-        Number(betsOnSizeNColors[this.numbers[7].size]) +
-        Number(betsOnSizeNColors[this.numbers[7].color[0]]),
-      8:
-        Number(betsOnSizeNColors[this.numbers[8].size]) +
-        Number(betsOnSizeNColors[this.numbers[8].color[0]]),
-      9:
-        Number(betsOnSizeNColors[this.numbers[9].size]) +
-        Number(betsOnSizeNColors[this.numbers[9].color[0]]),
-    };
-
-    const winningAmountOnNumbers = {
-      0:
-        betsOnNumbers[0] * numberMultiplier +
-        betsOnNumberByColorNSize[0] * defaultMultiplier,
-      1:
-        betsOnNumbers[1] * numberMultiplier +
-        betsOnNumberByColorNSize[1] * defaultMultiplier,
-      2:
-        betsOnNumbers[2] * numberMultiplier +
-        betsOnNumberByColorNSize[2] * defaultMultiplier,
-      3:
-        betsOnNumbers[3] * numberMultiplier +
-        betsOnNumberByColorNSize[3] * defaultMultiplier,
-      4:
-        betsOnNumbers[4] * numberMultiplier +
-        betsOnNumberByColorNSize[4] * defaultMultiplier,
-      5:
-        betsOnNumbers[5] * numberMultiplier +
-        betsOnNumberByColorNSize[5] * defaultMultiplier,
-      6:
-        betsOnNumbers[6] * numberMultiplier +
-        betsOnNumberByColorNSize[6] * defaultMultiplier,
-      7:
-        betsOnNumbers[7] * numberMultiplier +
-        betsOnNumberByColorNSize[7] * defaultMultiplier,
-      8:
-        betsOnNumbers[8] * numberMultiplier +
-        betsOnNumberByColorNSize[8] * defaultMultiplier,
-      9:
-        betsOnNumbers[9] * numberMultiplier +
-        betsOnNumberByColorNSize[9] * defaultMultiplier,
-    };
-
-    let lowestBetAmount = Infinity;
-
-    for (const [prediction, betAmount] of Object.entries(
-      winningAmountOnNumbers,
-    )) {
-      if (!isNaN(parseInt(prediction))) {
-        if (Number(betAmount) < lowestBetAmount) {
-          lowestBetAmount = Number(betAmount);
-        }
-      }
-    }
-
-    const possibleWinningNumbes: number[] = [];
-
-    Object.entries(winningAmountOnNumbers).forEach(
-      ([prediction, betAmount]) => {
-        if (Number(betAmount) === lowestBetAmount) {
-          possibleWinningNumbes.push(Number(prediction));
-        }
+    const betCount = await this.databaseService.bet.count({
+      where: {
+        gameId: game_id,
       },
-    );
+    });
 
-    return possibleWinningNumbes[
-      Math.floor(Math.random() * possibleWinningNumbes.length)
-    ];
+    if (betCount > 1) {
+      const defaultMultiplier = parseFloat(
+        process.env.DEFAULT_MULTIPLIER as string,
+      );
+      const numberMultiplier = parseFloat(
+        process.env.NUMBER_MULTIPLIER as string,
+      );
+
+      const betsOnNumbers = {
+        0: 0,
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0,
+        7: 0,
+        8: 0,
+        9: 0,
+      };
+
+      const betsOnSizeNColors = {
+        small: 0,
+        big: 0,
+        red: 0,
+        green: 0,
+        violet: 0,
+      };
+
+      const betAmountSum_groupBy_prediction =
+        await this.databaseService.bet.groupBy({
+          by: ['prediction'],
+          where: {
+            gameId: game_id,
+          },
+          _sum: {
+            amount: true,
+          },
+        });
+
+      for (const betGroup of betAmountSum_groupBy_prediction) {
+        if (isNaN(parseInt(betGroup.prediction))) {
+          betsOnSizeNColors[betGroup.prediction] = betGroup._sum.amount;
+        } else {
+          betsOnNumbers[betGroup.prediction] = betGroup._sum.amount;
+        }
+      }
+
+      const betsOnNumberByColorNSize = {
+        0:
+          Number(betsOnSizeNColors[this.numbers[0].size]) +
+          Number(betsOnSizeNColors[this.numbers[0].color[0]]) +
+          Number(betsOnSizeNColors[this.numbers[0].color[1]]),
+        1:
+          Number(betsOnSizeNColors[this.numbers[1].size]) +
+          Number(betsOnSizeNColors[this.numbers[1].color[0]]),
+        2:
+          Number(betsOnSizeNColors[this.numbers[2].size]) +
+          Number(betsOnSizeNColors[this.numbers[2].color[0]]),
+        3:
+          Number(betsOnSizeNColors[this.numbers[3].size]) +
+          Number(betsOnSizeNColors[this.numbers[3].color[0]]),
+        4:
+          Number(betsOnSizeNColors[this.numbers[4].size]) +
+          Number(betsOnSizeNColors[this.numbers[4].color[0]]),
+        5:
+          Number(betsOnSizeNColors[this.numbers[5].size]) +
+          Number(betsOnSizeNColors[this.numbers[5].color[0]]) +
+          Number(betsOnSizeNColors[this.numbers[5].color[1]]),
+        6:
+          Number(betsOnSizeNColors[this.numbers[6].size]) +
+          Number(betsOnSizeNColors[this.numbers[6].color[0]]),
+        7:
+          Number(betsOnSizeNColors[this.numbers[7].size]) +
+          Number(betsOnSizeNColors[this.numbers[7].color[0]]),
+        8:
+          Number(betsOnSizeNColors[this.numbers[8].size]) +
+          Number(betsOnSizeNColors[this.numbers[8].color[0]]),
+        9:
+          Number(betsOnSizeNColors[this.numbers[9].size]) +
+          Number(betsOnSizeNColors[this.numbers[9].color[0]]),
+      };
+
+      const winningAmountOnNumbers = {
+        0:
+          betsOnNumbers[0] * numberMultiplier +
+          betsOnNumberByColorNSize[0] * defaultMultiplier,
+        1:
+          betsOnNumbers[1] * numberMultiplier +
+          betsOnNumberByColorNSize[1] * defaultMultiplier,
+        2:
+          betsOnNumbers[2] * numberMultiplier +
+          betsOnNumberByColorNSize[2] * defaultMultiplier,
+        3:
+          betsOnNumbers[3] * numberMultiplier +
+          betsOnNumberByColorNSize[3] * defaultMultiplier,
+        4:
+          betsOnNumbers[4] * numberMultiplier +
+          betsOnNumberByColorNSize[4] * defaultMultiplier,
+        5:
+          betsOnNumbers[5] * numberMultiplier +
+          betsOnNumberByColorNSize[5] * defaultMultiplier,
+        6:
+          betsOnNumbers[6] * numberMultiplier +
+          betsOnNumberByColorNSize[6] * defaultMultiplier,
+        7:
+          betsOnNumbers[7] * numberMultiplier +
+          betsOnNumberByColorNSize[7] * defaultMultiplier,
+        8:
+          betsOnNumbers[8] * numberMultiplier +
+          betsOnNumberByColorNSize[8] * defaultMultiplier,
+        9:
+          betsOnNumbers[9] * numberMultiplier +
+          betsOnNumberByColorNSize[9] * defaultMultiplier,
+      };
+
+      let lowestBetAmount = Infinity;
+
+      for (const [prediction, betAmount] of Object.entries(
+        winningAmountOnNumbers,
+      )) {
+        if (!isNaN(parseInt(prediction))) {
+          if (Number(betAmount) < lowestBetAmount) {
+            lowestBetAmount = Number(betAmount);
+          }
+        }
+      }
+
+      const possibleWinningNumbes: number[] = [];
+
+      Object.entries(winningAmountOnNumbers).forEach(
+        ([prediction, betAmount]) => {
+          if (Number(betAmount) === lowestBetAmount) {
+            possibleWinningNumbes.push(Number(prediction));
+          }
+        },
+      );
+
+      return possibleWinningNumbes[
+        Math.floor(Math.random() * possibleWinningNumbes.length)
+      ];
+    } else {
+      const possibleWinningNumbes: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+      return possibleWinningNumbes[
+        Math.floor(Math.random() * possibleWinningNumbes.length)
+      ];
+    }
   }
 
   async create(createGameDto: CreateGameDto) {
